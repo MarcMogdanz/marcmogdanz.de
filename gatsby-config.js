1c9c77c5bbbe0928892e30927ce2bb551a47029c
MarcMogdanz <info@minekayama.de>
2019-08-10 19:25:33 +0200

    remove useless stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,22 +52,11 @@ module.exports = {
       resolve: "gatsby-plugin-google-analytics",
       options: {
         trackingId: "UA-145435399-1",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
-        // Setting this parameter is optional
         anonymize: true,
-        // Setting this parameter is also optional
         respectDNT: true,
-        // Avoids sending pageview hits from custom paths
         exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
         pageTransitionDelay: 0,
-        // Set Variation ID. 0 for original 1,2,3....
-        //variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Any additional optional fields
-        //sampleRate: 5,
-        //siteSpeedSampleRate: 10,
-        //cookieDomain: "example.com",
       },
     },
   ],
