c22e711b4c3b90a63429d1dc86ce6082eeec3d58
Marc Mogdanz <hey@marcmogdanz.de>
2023-08-09 01:00:03 +0200

    chore: remove time on blog detail page for now

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -36,17 +36,17 @@ const FormattedDatetime = ({ datetime }: { datetime: string | Date }) => {
     day: "numeric",
   });
 
-  const time = myDatetime.toLocaleTimeString(LOCALE, {
-    hour: "2-digit",
-    minute: "2-digit",
-  });
+  // const time = myDatetime.toLocaleTimeString(LOCALE, {
+  //   hour: "2-digit",
+  //   minute: "2-digit",
+  // });
 
   return (
     <>
       {date}
-      <span aria-hidden="true"> | </span>
+      {/* <span aria-hidden="true"> | </span>
       <span className="sr-only">&nbsp;at&nbsp;</span>
-      {time}
+      {time} */}
     </>
   );
 };
